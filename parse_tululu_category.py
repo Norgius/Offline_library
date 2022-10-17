3a6bc259f7d09cee2b3fb8834b376fed5f2966b1
Norgius <dmitry.schadrin21@yandex.ru>
2022-10-17 20:40:22 +0300

    Minor edits and added support for download control

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -11,6 +11,7 @@ from time import sleep
 from main import check_for_redirect, parse_book_page, save_text, download_image
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 ENCODING = 'UTF-8'
 logger = logging.getLogger(__file__)
@@ -27,7 +28,7 @@ def create_json_file_with_books(books, dest_folder, json_path):
 
 def get_books(url, book_ids, skip_imgs, skip_txt, dest_folder):
     books = []
-    for book_id in book_ids:
+    for book_id in tqdm(book_ids):
         params = {'id': book_id}
         try:
             response = requests.get(url=f'{url}txt.php',
@@ -49,7 +50,6 @@ def get_books(url, book_ids, skip_imgs, skip_txt, dest_folder):
                 img_link = urljoin(html_book_page.url, book.get('img_src'))
                 img_file_path = download_image(img_link, book_id, dest_folder)
                 book['img_src'] = img_file_path
-            books.append(book)
         except requests.exceptions.HTTPError as http_er:
             logger.info(f'Невозможно загрузить книгу по данному '
                         f'book_id = {book_id}\n{http_er}\n')
@@ -61,6 +61,9 @@ def get_books(url, book_ids, skip_imgs, skip_txt, dest_folder):
             sys.stderr.write(f'{connect_er}\n\n')
             sleep(15)
             continue
+        books.append(book)
+        print(f'Название: {book.get("title")}')
+        print(f'Автор: {book.get("author")}\n')
     return books
 
 
@@ -108,14 +111,14 @@ def main():
     parser = argparse.ArgumentParser(
         description='Скачивает книги в указанном диапазоне'
     )
-    parser.add_argument('--start_page', type=int,
+    parser.add_argument('--start_page', default=700, type=int,
                         help='Начало диапазона')
     parser.add_argument('--end_page', default=702, type=int,
                         help='Конец диапазона')
     parser.add_argument('--skip_imgs', action='store_true', default=False,
-                        help='Нужно ли скачивать обложки книг?')
+                        help='Нужно ли исключить скачивание обложек книг?')
     parser.add_argument('--skip_txt', action='store_true', default=False,
-                        help='Нужно ли скачивать текст книг?')
+                        help='Нужно ли исключить скачивание текста книг?')
     parser.add_argument('--json_path', default='', type=str,
                         help='Путь к *.json файлу с результатами')
     parser.add_argument('--dest_folder', default='', type=str,
