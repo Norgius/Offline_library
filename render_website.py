a88d3c4b64ea3620be4db8c2766d37ca388a7f0c
Norgius <dmitry.schadrin21@yandex.ru>
2022-10-27 17:35:07 +0300

    Combed the code

--- render_website.py
+++ render_website.py
@@ -1,22 +1,15 @@
 import os
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 from pathlib import Path
 
 from more_itertools import chunked
-from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-html_pages_folder = 'pages'
+ENCODING = 'UTF-8'
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-def rebuild():
+def create_offline_website(env, html_pages_folder):
     template = env.get_template('template.html')
-    Path(html_pages_folder).mkdir(parents=True, exist_ok=True)
     with open('books.json', 'r') as file:
         books = json.load(file)
     book_pages = list(chunked(books, 20))
@@ -24,14 +17,25 @@ def rebuild():
 
     for number, book_page in enumerate(book_pages, 1):
         books = list(chunked(book_page, len(book_page) // 2))
-        rendered_page = template.render(books=books, number_pages=number_pages, current_page=number)
+        rendered_page = template.render(
+            books=books,
+            number_pages=number_pages,
+            current_page=number
+        )
         filepath = os.path.join(html_pages_folder, f'index{number}.html')
-        with open(filepath, 'w', encoding="utf8") as file:
+        with open(filepath, 'w', encoding=ENCODING) as file:
             file.write(rendered_page)
-    print("Site rebuilt")
 
 
-rebuild()
-server = Server()
-server.watch('template.html', rebuild)
-server.serve(root='pages/', host='127.0.0.1', port=5500)
+def main():
+    html_pages_folder = 'pages'
+    Path(html_pages_folder).mkdir(parents=True, exist_ok=True)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    create_offline_website(env, html_pages_folder)
+    print('Сайт библиотеки создан')
+
+if __name__ == '__main__':
+    main()
