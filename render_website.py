14f7fa4fa687078e38e9630066b8d84a2776c354
Norgius <dmitry.schadrin21@yandex.ru>
2022-10-29 11:04:21 +0300

    Naming variable and connect the numbers with code

--- render_website.py
+++ render_website.py
@@ -8,18 +8,18 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 ENCODING = 'UTF-8'
 
 
-def create_offline_website(env, html_pages_folder):
+def create_offline_website(env, html_pages_folder, books_number_per_page):
     template = env.get_template('template.html')
     with open('books.json', 'r') as file:
         books = json.load(file)
-    book_pages = list(chunked(books, 20))
-    number_pages = len(book_pages)
+    book_pages = list(chunked(books, books_number_per_page))
+    pages_number = len(book_pages)
 
     for number, book_page in enumerate(book_pages, 1):
-        books = list(chunked(book_page, len(book_page) // 2))
+        books_per_page = list(chunked(book_page, len(book_page) // 2))
         rendered_page = template.render(
-            books=books,
-            number_pages=number_pages,
+            books=books_per_page,
+            pages_number=pages_number,
             current_page=number
         )
         filepath = os.path.join(html_pages_folder, f'index{number}.html')
@@ -29,12 +29,13 @@ def create_offline_website(env, html_pages_folder):
 
 def main():
     html_pages_folder = 'pages'
+    books_number_per_page = 20
     Path(html_pages_folder).mkdir(parents=True, exist_ok=True)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    create_offline_website(env, html_pages_folder)
+    create_offline_website(env, html_pages_folder, books_number_per_page)
     print('Сайт библиотеки создан')
 
 if __name__ == '__main__':
